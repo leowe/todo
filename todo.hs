93a9175ffeefad6831141298827c0ef82f0ba715
Leo Wendt <leowndt@gmail.com>
2016-06-12 20:17:51 +0200

    added functionality

---
+++ todo.hs
@@ -0,0 +1,61 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [ ("add", add)
+           , ("view", view)
+           , ("remove", remove)
+           , ("prioritize", prioritize)
+           , ("help", help)
+           ]
+
+main = do
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action args
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+      contents <- readFile fileName
+      let todoTasks = lines contents
+          numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+      putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    handle <- openFile fileName ReadMode
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    contents <- hGetContents handle
+    let number = read numberString
+        todoTasks = lines contents
+        newTodoItems = delete (todoTasks !! number) todoTasks
+    hPutStr tempHandle $ unlines newTodoItems
+    hClose handle
+    hClose tempHandle
+    removeFile fileName
+    renameFile tempName fileName
+
+prioritize :: [String] -> IO ()
+prioritize [fileName, numberString] = do
+    handle <- openFile fileName ReadMode
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    contents <- hGetContents handle
+    let number = read numberString
+        todoTasks = lines contents
+        todoTask = todoTasks !! number
+        newTodoItems = todoTask : (delete todoTask todoTasks)
+    hPutStr tempHandle $ unlines newTodoItems
+    hClose handle
+    hClose tempHandle
+    removeFile fileName
+    renameFile tempName fileName
+
+help :: [String] -> IO ()
+help []  = do print $ map fst dispatch
+              print "args ordering: filename rest_of_args"  
+
